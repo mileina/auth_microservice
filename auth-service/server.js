b719a70a3c82d555532ab385f4bf6130b52659d1
mileina <meas.pinya@gmail.com>
2025-07-09 14:36:22 +0200

    Refactor: Extract MongoDB connection into config file

--- auth-service/server.js
+++ auth-service/server.js
@@ -1,19 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
+connectDB();
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
-
-mongoose.connect(MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/auth', authRoutes);
 
